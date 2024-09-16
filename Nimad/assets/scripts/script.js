a1b24d14a0b1e3e9d6ee9851d7f9bbc907c450b8
Mohammadreza <mo.khorshidi80@gmail.com>
2024-09-16 10:14:44 +0330

    fix header swiper

--- Nimad/assets/scripts/script.js
+++ Nimad/assets/scripts/script.js
@@ -1,19 +1,24 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
   const headerSwiper = new Swiper('.header-swiper', {
     loop: true,
     slidesPerView: 4,
-    spaceBetween: 20, 
+    spaceBetween: 20,
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true
+    },
+    speed: 1000,
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
     },
     // autoplay: {
     //   delay: 5000,
     //   disableOnInteraction: false,
     // },
     breakpoints: {
-      200:{
+      200: {
         slidesPerView: 1,
       },
       640: {
@@ -30,70 +35,69 @@ document.addEventListener("DOMContentLoaded", function() {
       },
     },
   });
-    
-  
+
+
   const swiper = new Swiper('.blog-swiper', {
-      loop: true,
-      slidesPerView: 4,
-      spaceBetween: 20, 
-      speed: 1500,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 20,
+    speed: 1500,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      200: {
+        slidesPerView: 1,
       },
-      autoplay: {
-        delay: 5000,
-        disableOnInteraction: false,
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 10,
       },
-      breakpoints: {
-        200:{
-          slidesPerView: 1,
-        },
-        640: {
-          slidesPerView: 2,
-          spaceBetween: 10,
-        },
-        768: {
-          slidesPerView: 4,
-          spaceBetween: 20,
-        },
-        1024: {
-          slidesPerView: 4,
-          spaceBetween: 20,
-        },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 20,
       },
-    });
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+    },
+  });
 
 
-    const projectSwiper = new Swiper('.project-swiper', {
-      loop: true,
-      slidesPerView: 4,
-      spaceBetween: 20, 
-      navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
+  const projectSwiper = new Swiper('.project-swiper', {
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    // autoplay: {
+    //   delay: 5000,
+    //   disableOnInteraction: false,
+    // },
+    breakpoints: {
+      200: {
+        slidesPerView: 1,
       },
-      // autoplay: {
-      //   delay: 5000,
-      //   disableOnInteraction: false,
-      // },
-      breakpoints: {
-        200:{
-          slidesPerView: 1,
-        },
-        640: {
-          slidesPerView: 2,
-          spaceBetween: 10,
-        },
-        768: {
-          slidesPerView: 4,
-          spaceBetween: 20,
-        },
-        1024: {
-          slidesPerView: 4,
-          spaceBetween: 20,
-        },
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 10,
       },
-    });
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+    },
+  });
 });
-  
